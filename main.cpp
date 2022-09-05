60ccf130e66b2e209e6cc0cda78913be35fdf1ef
lobanovam <71985042+lobanovam@users.noreply.github.com>
2022-09-05 21:05:36 +0300

    devided in func, everything is working fine

--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@
 #include <ctype.h>
 #include <assert.h>
 size_t getline (char **s, size_t *n, FILE *stream);
-const int defaultBuffSize = 10;
 int my_strcmp (const char *s1, const char *s2);
+int isAlpha(const int ch);
+void sortLines(char **arr, int lines_count);
+void printArr(char **arr, int lines_count, FILE *write);
+void readFile(char ***arr, size_t *lines_count_pt, size_t *arr_size_pt, FILE *read);
+
+const int defaultBuffSize = 10;
 
 int main() {
     FILE *read = NULL;
@@ -13,7 +18,7 @@ int main() {
     size_t lines_count = 0;
     size_t arr_size = defaultBuffSize;
 
-    if ((read = fopen("test.txt", "r")) == NULL) {
+    if ((read = fopen("onegin.txt", "r")) == NULL) {
         printf("cant't open file\n");
         return 0;
     }
@@ -24,59 +29,23 @@ int main() {
     arr = (char**)(calloc(arr_size, sizeof(char*)));
     printf("calloc done with pointer %p\n\n", arr );
 
-
-
-
-    while (!feof(read)) {
-        size_t n = 5;
-        char *line = nullptr;
-
-        line = (char*)(calloc(n, sizeof(char)));
-
-        n = getline(&line, &n, read);
-        if (line[0] == '\0')
-            continue;
-
-        arr[lines_count] = line;
-        printf("%s of len %d was read\n", arr[lines_count], n);
-        lines_count++;
-
-        if (lines_count == arr_size) {
-            arr_size *= 2;
-            printf("\ntrying to realloc with pointer %p\n", arr);
-            arr = (char**)(realloc(arr, arr_size * sizeof(char*)));
-            printf("realloc done with pointer %p\n\n", arr );
-
-        }
-
-    }
+    readFile(&arr, &lines_count, &arr_size, read);
 
     fclose(read);
-    printf("\nread file is closed. total lines (not empty) %d\n", lines_count);
+    printf("\nread file is closed. total lines (not empty) %d, pointer %p\n", lines_count, arr);
 
-    for (int i = 0; i < lines_count; i++) {
-        for (int j = i + 1; j < lines_count; j++) {
-            if (my_strcmp(arr[i], arr[j]) > 0) {
-                char* str = arr[i];
-                arr[i] = arr[j];
-                arr[j] = str;
-            }
-         }
-    }
+    sortLines(arr, lines_count);
     printf("sort completed\n");
 
     if ((write = fopen("result.txt", "w")) == NULL) {
         printf("cant't open file\n");
         return 0;
     }
+    printArr(arr, lines_count, write);
 
-    for (int i = 0; i < lines_count; i++) {
-        fputs(arr[i], write);
-        fputs("\n", write);
-    }
     free(arr);
     fclose(write);
-    printf("write file is closed. Sort completed\n");
+    printf("write file is closed. Job is done\n");
 
 
     return 0;
@@ -90,33 +59,19 @@ size_t getline (char **s, size_t *n, FILE *fp) {
         *s = (char*)(calloc(*n, sizeof(char)));
     }
     int ch = 0;
-    //printf("\n\ngetline is working\n");
     size_t i = 0;
     while ((ch = fgetc(fp))!= '\n' && ch != EOF) {
         (*s)[i] = ch;
-        //printf("(*s)[%d] is %c\n",i, (*s)[i]);
         i++;
-        //printf("i is %d and n is %d\n", i, *n);
+
         if (i == *n) {
-
-            //printf("i exceeding n, time to realloc\n");
             *n *= 2;
-            //size_t len = strlen(*s);
-           // printf("Trying Realloc for size %d with ptr = %p\n", *n, *s);
-             *s= (char*)(realloc(*s, *n * sizeof(char)));
-           // printf("Realloc finished for size %d with ptr = %p\n", *n, *s);
-
-            //printf("strlen finished\n");
-
+            *s= (char*)(realloc(*s, *n * sizeof(char)));
             (*s)[i] = '\0';
-            //printf("adding 0 finished\n");
 
         }
     }
     (*s)[i] = '\0';
-    //*s = (char*)(realloc(*s, strlen(*s)));
-    //printf("final len is %d\n", strlen(*s));
-    //printf("final version is %s\n", *s);
     *n = i;
     return *n;
 }
@@ -125,27 +80,80 @@ int my_strcmp (const char *s1, const char *s2) {
     assert(s1 != nullptr);
     assert(s2 != nullptr);
 
-    while (!isalpha(*s1)) {
+    while (!isAlpha(*s1) && *s1 != '\0') {
                 s1++;
         }
-    while (!isalpha(*s2)) {
+    while (!isAlpha(*s2) && *s2 != '\0') {
                 s2++;
         }
 
     while (*s1 == *s2 && *s1 != '\0' ) {
             s1++;
             s2++;
-            while (!isalpha(*s1) && *s1 != '\0') {
+            while (!isAlpha(*s1) && *s1 != '\0') {
                 s1++;
             }
-            while (!isalpha(*s2) && *s2 != '\0') {
+            while (!isAlpha(*s2) && *s2 != '\0') {
                 s2++;
             }
-
         }
 
 return (*s1 - *s2);
 }
+
+int isAlpha(const int ch) {
+    if (ch >= -64 && ch <= -1 || ch == 72 || isalpha(ch)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void sortLines(char **arr, int lines_count) {
+
+    for (int i = 0; i < lines_count; i++) {
+        for (int j = i + 1; j < lines_count; j++) {
+            if (my_strcmp(arr[i], arr[j]) > 0) {
+                char* str = arr[i];
+                arr[i] = arr[j];
+                arr[j] = str;
+            }
+         }
+    }
+}
+
+void printArr(char **arr, int lines_count, FILE *write) {
+
+    for (int i = 0; i < lines_count; i++) {
+        fputs(arr[i], write);
+        fputs("\n", write);
+    }
+}
+
+void readFile(char ***arr_pt, size_t *lines_count_pt, size_t *arr_size_pt, FILE *read) {
+
+    while (!feof(read)) {
+
+        size_t n = defaultBuffSize;
+        (*arr_pt)[*lines_count_pt] = (char*)(calloc(n, sizeof(char)));
+
+        n = getline(&(*arr_pt)[*lines_count_pt], &n, read);
+
+        if ((*arr_pt)[*lines_count_pt][0] == '\0')
+            continue;
+
+        (*lines_count_pt)++;
+
+        if (*lines_count_pt == *arr_size_pt) {
+            (*arr_size_pt) *= 2;
+            printf("\ntrying to realloc with pointer %p\n", *arr_pt);
+            *arr_pt = (char**)(realloc(*arr_pt, *arr_size_pt * sizeof(char*)));
+            printf("realloc done with pointer %p\n\n", *arr_pt );
+        }
+    }
+}
+
+
 
 
 
