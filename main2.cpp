2ad08c71b99cac4cfc54561bf7f44e7f23c6b5e8
lobanovam <71985042+lobanovam@users.noreply.github.com>
2022-09-19 21:36:10 +0300

    started struct, but still removable

--- main2.cpp
+++ main2.cpp
@@ -3,69 +3,87 @@
 #include <string.h>
 #include <ctype.h>
 #include <sys\stat.h>
-#include <assert.h>
 #include <stdbool.h>
-int my_strcmp (const char *s1, const char *s2);
-int inverse_strcmp(const char *s1, const char *s2);
-int isAlpha(const int ch);
-void sortLines(char **arr, int lines_count, bool inverted);
-void printArr(char **arr, int lines_count, FILE *write);
-void connectPointers(char ***pointers_pt, char *buff, size_t *lines_count, size_t *pointersSize, int ch_count);
-void printBuff (char *buff, int ch_count, FILE *write);
+#include "log.h"
+#include <assert.h>
 
+int my_strcmp (const void *s1, const void *s2);
+int inverse_strcmp(const void *s1, const void *s2);
+int isAlpha(const int ch);
+void sortLines(char **arr, size_t lines_count, int (*comp)(const void*, const void*));
+void printArr(char **arr, int lines_count, FILE *write);
+void connectPointers(struct Text* onegin_ptr);
+void printBuff (char *buff, int ch_count, FILE *write);
+void _swap(char* s1, char* s2);
+void readFile(struct Text* onegin_ptr);
 
 const size_t defaultSize = 10;
 
-int main() {
+struct Text {
+    char *buff;
+    char **pointers;
+    size_t pointersSize;
+    size_t ch_count;
+    size_t lines_count;
+};
 
-    size_t pointersSize = defaultSize;
-    struct stat file;
-    stat("onegin.txt", &file);
+//typedef struct Text Text;
+const char *FILENAME = "onegin.txt";
+
+int main() {
+    struct Text onegin;
+
+    //size_t pointersSize = defaultSize;
+    /*struct stat file;
+    stat(FILENAME, &file);
     size_t buffSize = file.st_size;
-    printf("buffSize is %ld\n", buffSize);
+    fprintf(LogFile,"buffSize is %ld\n", buffSize);
 
     FILE *read = NULL;
-    FILE *write = NULL;
 
-    if ((read = fopen("onegin.txt", "r")) == NULL) {
-        printf("cant't open file\n");
+    if ((read = fopen(FILENAME, "r")) == NULL) {
+        fprintf(LogFile,"cant't open file\n");
         return 0;
-    }
+    }*/
 
-    char *buff = nullptr;
-    char **pointers = nullptr;
+    onegin.buff = (char*)(calloc(buffSize, sizeof(char)));
+    onegin.pointers = (char**)(calloc(pointersSize, sizeof(char*)));
+    onegin.pointersSize = defaultSize;
+    //ASSERT(buff != NULL);
+    //ASSERT(pointers != NULL);
 
-    buff = (char*)(calloc(buffSize, sizeof(char)));
-    printf("buff calloc done with pointer %p\n", buff);
+    readFile(&onegin);
 
-    pointers = (char**)(calloc(pointersSize, sizeof(char*)));
-    printf("pointers calloc done with pointer %p\n", pointers);
 
-    size_t ch_count = fread(buff, sizeof(char), buffSize, read);
-    fclose(read);
+    //size_t lines_count = 0;
 
-    size_t lines_count = 0;
+    connectPointers(&onegin);
 
-    connectPointers(&pointers, buff, &lines_count, &pointersSize, ch_count);
+    fprintf(LogFile,"lines_count is %ld\n", lines_count);
 
-    printf("lines_count is %ld\n", lines_count);
+    FILE *write = NULL;
 
     if ((write = fopen("result.txt", "w")) == NULL) {
         printf("cant't open file\n");
         return 0;
     }
+    //ASSERT(write != NULL);
 
-    bool inverted = false;
-    printf("sorting normally\n");
-    sortLines(pointers, lines_count, inverted);
+    fprintf(LogFile,"sorting normally\n");
+    qsort(pointers, lines_count, sizeof(char*), (int(*) (const void *, const void *)) my_strcmp);
+    //sortLines(pointers, lines_count, (int(*) (const void*, const void*)) my_strcmp);
     printArr(pointers, lines_count, write);
-    printf("normal sort completed\n");
+    fprintf(LogFile, "normal sort completed\n");
 
-    inverted = true;
-    printf("sorting inverted\n");
-    sortLines(pointers, lines_count, inverted);
+    fprintf(write, "\n\n now inversed\n\n");
+
+    fprintf(LogFile,"sorting inverted\n");
+    qsort(pointers, lines_count, sizeof(char*), (int(*) (const void *, const void *)) inverse_strcmp);
+    //sortLines(pointers, lines_count, (int(*) (const void*, const void*)) inverse_strcmp);
     printArr(pointers, lines_count, write);
-    printf("inverted sort completed\n");
+    fprintf(LogFile,"inverted sort completed\n");
+
+    fprintf(write, "\n\n now based\n\n");
 
     printBuff(buff, ch_count, write);
 
@@ -79,54 +97,44 @@ int main() {
 
 }
 
-void sortLines(char **arr, int lines_count, bool inverted) {
-    if(inverted) {
-        for (int i = 0; i < lines_count; i++) {
-            for (int j = i + 1; j < lines_count; j++) {
-                if (inverse_strcmp(arr[i], arr[j]) > 0) {
-                    char* str = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = str;
-                }
-            }
-        }
-    } else {
-        for (int i = 0; i < lines_count; i++) {
-            for (int j = i + 1; j < lines_count; j++) {
-                if (my_strcmp(arr[i], arr[j]) > 0) {
-                    char* str = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = str;
-                }
+void sortLines(char **arr, size_t lines_count, int (*comp)(const void*, const void*)) {
+    for (size_t i = 0; i < lines_count; i++) {
+        for (size_t j = i + 1; j < lines_count; j++) {
+            if (comp(&arr[i], &arr[j]) > 0) {
+                _swap(arr[i], arr[j]);
             }
         }
     }
 }
 
-int my_strcmp (const char *s1, const char *s2) {
-    assert(s1 != nullptr);
-    assert(s2 != nullptr);
+int my_strcmp (const void *s1, const void *s2) {
+    //ASSERT(s1 != NULL);
+    //ASSERT(s2 != NULL);
 
+    char* str1 = *((char**) s1);
+    char* str2 = *((char**) s2);
 
-    while (!isAlpha(*s1) && *s1 != '\0') {
-                s1++;
+    while (!isAlpha(*str1) && *(str1) != '\0') {
+                str1 += sizeof(char);
         }
-    while (!isAlpha(*s2) && *s2 != '\0') {
-                s2++;
+    while (!isAlpha(*str2) && *(str2) != '\0') {
+                str2 += sizeof(char);
         }
 
-    while (*s1 == *s2 && *s1 != '\0' ) {
-            s1++;
-            s2++;
-            while (!isAlpha(*s1) && *s1 != '\0') {
-                s1++;
+    while ((*str1 == *str2) && (*str1 != '\0' )) {
+            str1 += sizeof(char);
+            str2 += sizeof(char);
+
+            while (!isAlpha(*str1) && *(str1) != '\0') {
+                str1 += sizeof(char);
             }
-            while (!isAlpha(*s2) && *s2 != '\0') {
-                s2++;
+            while (!isAlpha(*str2) && *(str2) != '\0') {
+                str2 += sizeof(char);
             }
+
         }
 
-return (*s1 - *s2);
+return (*str1 - *str2);
 }
 
 int isAlpha(const int ch) {
@@ -138,54 +146,66 @@ int isAlpha(const int ch) {
 }
 
 void printArr(char **arr, int lines_count, FILE *write) {
+    //ASSERT(arr != NULL);
+   // ASSERT(write != NULL);
 
-    for (int i = 0; i < lines_count; i++) {
+    for (size_t i = 0; i < lines_count; i++) {
         fputs(arr[i], write);
         fputs("\n", write);
     }
 }
 
 void connectPointers(char ***pointers_pt, char *buff, size_t *lines_count_pt, size_t *pointersSize_pt, int ch_count){
+    //ASSERT(pointers_pt != NULL);
+    //ASSERT(buff != NULL);
+    //ASSERT(lines_count_pt != NULL);
+    //ASSERT(pointersSize_pt != NULL);
+
     **pointers_pt = buff;
-    for (int i = 1, j = 1; i < ch_count; i++) {
+    for (size_t i = 1, j = 1; i < ch_count; i++) {
         if (buff[i] == '\n') {
             (*lines_count_pt)++;
+
             buff[i] = '\0';
             (*pointers_pt)[j++] = &(buff[i+1]);
+
             if (j == *pointersSize_pt) {
                 *pointersSize_pt *= 2;
-                printf("trying to realloc with pointer %p\n", *pointers_pt);
+                fprintf(LogFile,"trying to realloc with pointer %p\n", *pointers_pt);
                 *pointers_pt = (char**)(realloc(*pointers_pt, *pointersSize_pt * sizeof(char*)));
-                printf("realloc done with pointer %p\n", *pointers_pt);
+                fprintf(LogFile,"realloc done with pointer %p\n", *pointers_pt);
             }
         }
     }
 }
 
-int inverse_strcmp(const char *s1, const char *s2){
-    assert(s1 != nullptr);
-    assert(s2 != nullptr);
+int inverse_strcmp(const void *s1, const void *s2){
+    //ASSERT(s1 != NULL);
+    //ASSERT(s2 != NULL);
 
-    size_t len1 = strlen(s1);
-    size_t len2 = strlen(s2);
+    char* str1 = *((char**) s1);
+    char* str2 = *((char**) s2);
 
-    const char *v1 = s1 + len1 - 1;
-    const char *v2 = s2 + len2 - 1;
+    size_t len1 = strlen(str1);
+    size_t len2 = strlen(str2);
 
-    while (!isAlpha(*v1) && v1 > s1) {
+    const char *v1 = str1 + len1 - 1;
+    const char *v2 = str2 + len2 - 1;
+
+    while (!isAlpha(*v1) && v1 > str1) {
                 v1--;
         }
-    while (!isAlpha(*v2) && v2 > s2) {
+    while (!isAlpha(*v2) && v2 > str2) {
                 v2--;
         }
 
-        while (*v1 == *v2 && v1 > s1 && v2 > s2) {
+        while (*v1 == *v2 && v1 > str1 && v2 > str2) {
             v1--;
             v2--;
-            while (!isAlpha(*v1) && v1 > s1) {
+            while (!isAlpha(*v1) && v1 > str1) {
                 v1--;
             }
-            while (!isAlpha(*v2) && v2 > s2) {
+            while (!isAlpha(*v2) && v2 > str2) {
                 v2--;
             }
         }
@@ -194,7 +214,9 @@ int inverse_strcmp(const char *s1, const char *s2){
 }
 
 void printBuff (char *buff, int ch_count, FILE *write) {
-    for (int i = 0; i < ch_count; i++) {
+   //ASSERT(buff != NULL);
+   // ASSERT(write != NULL);
+    for (size_t i = 0; i < ch_count; i++) {
         if (buff[i] == '\0') {
             fputc('\n', write);
         } else {
@@ -202,3 +224,31 @@ void printBuff (char *buff, int ch_count, FILE *write) {
         }
     }
 }
+
+void _swap(char *s1, char *s2) {
+   // ASSERT(s1 != NULL);
+    //ASSERT(s2 != NULL);
+
+    char* str = s1;
+    s1 = s2;
+    s2 = str;
+}
+
+void readFile(struct Text *onegin_ptr) {
+
+    struct stat file;
+    stat(FILENAME, &file);
+    size_t buffSize = file.st_size;
+
+    FILE *read = NULL;
+    if ((read = fopen(FILENAME, "r")) == NULL) {
+        fprintf(LogFile,"cant't open file\n");
+        return 0;
+    }
+    onegin_ptr->ch_count = fread(onegin_ptr->buff, sizeof(char), buffSize, read);
+    fclose(read);
+
+
+
+}
+
